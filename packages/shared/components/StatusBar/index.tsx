ba042c5ba30e880b1d6f2cdda2c6a626ba022ca7
Arthur Santos <silva.arthur@academico.ifpb.edu.br>
2020-10-11 17:44:26 -0300

    feat: add status shared folder project

---
+++ packages/shared/components/StatusBar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const STATUS_BAR_HEIGHT = 22
+
+import { Container } from './styles';
+
+const StatusBar: React.FC = () => {
+  return <Container />;
+}
+
+export {StatusBar};
---
+++ packages/shared/components/StatusBar/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  grid-area: STTB;
+
+  background: #007ACC;
+  border-top: 1px solid #1c1e1f;
+`;
